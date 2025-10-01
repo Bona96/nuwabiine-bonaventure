95b510520968bdc9dd5f188c87c9f000aae858e7
KNiHT Technologies <knihttechnologies@gmail.com>
2025-10-01 19:23:12 +0300

    Adding dark mode to CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const CTA = () => {
   return (
     <section className='cta'>
-      <p className='cta-text'>
+      <p className='cta-text text-gray-800 dark:text-gray-200'>
         Have a project in mind? <br className='sm:block hidden' />
         Let’s build something together!
       </p>
-      <Link to='/contact' className='btn'>
+      <Link to='/contact' className='btn text-gray-800 dark:text-gray-200'>
         Contact
       </Link>
     </section>
